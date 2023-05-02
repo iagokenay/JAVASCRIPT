a41f63d358a4dc9a698e2945973b1e3116cb5c38
iagokenay <113652236+iagokenay@users.noreply.github.com>
2023-05-02 09:38:24 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel  = 60
+console.log(`a velocidade do seu carro e de ${vel}km/h`)
+if (vel > 60){///condição simplis 
+    console.log(`você foi multado`)
+}
+console.log(`dirija sempre com cinto de segurança!`)
---
+++ ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+
+if (país == 'Brasil'){
+    console.log("Você e brasileiro")
+}
+else{
+    console.log('Você e estranjeiro')
+}
